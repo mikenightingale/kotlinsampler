d376598fa7a742bb1217455bc717c3cbd275792f
Michael Nightingale <michaeln.public@icloud.com>
2024-08-21 11:48:37 +0100

    chore: initial commit

---
+++ src/main/kotlin/com/miken/kotlinsampler/KotlinsamplerApplication.kt
@@ -0,0 +1,11 @@
+package com.miken.kotlinsampler
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinsamplerApplication
+
+fun main(args: Array<String>) {
+	runApplication<KotlinsamplerApplication>(*args)
+}
---
+++ src/test/kotlin/com/miken/kotlinsampler/KotlinsamplerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.miken.kotlinsampler
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinsamplerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
